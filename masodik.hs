587cb6358bb18bbe894d414527b112c7b0002ca5
hunorszegedi <szegedi.hunor@student.ms.sapientia.ro>
2024-03-06 15:55:02 +0200

    szam szorzata mindket valtozat

--- masodik.hs
+++ masodik.hs
@@ -1,4 +1,5 @@
-import qualified Control.Applicative as A
+import Control.Applicative qualified as A
+
 -- 1.feladat
 -- Könyvtárfüggvények használata nélkül, definiáljuk azt a függvényt, amely meghatározza:
 -- egy szám számjegyeinek szorzatát (2 módszerrel)
@@ -20,7 +21,8 @@ szamSzemjegyeinekOsszege :: Int -> Int
 szamSzemjegyeinekOsszege szam
   | szam < 10 = szam
   | otherwise = (szam `mod` 10) + szamSzemjegyeinekOsszege (szam `div` 10)
---masik modszer
+
+-- masik modszer
 szamSzemjegyeinekOsszege2 :: Int -> Int
 szamSzemjegyeinekOsszege2 szam = if szam < 10 then szam else (szam `mod` 10) + szamSzemjegyeinekOsszege2 (szam `div` 10)
 
@@ -30,22 +32,36 @@ szamSzamjegyeinekSzama :: Int -> Int
 szamSzamjegyeinekSzama szam
   | szam < 10 = 1
   | otherwise = 1 + szamSzamjegyeinekSzama (szam `div` 10)
---masik modszer
+
+-- masik modszer
 szamSzamjegyeinekSzama2 :: Int -> Int
 szamSzamjegyeinekSzama2 szam = if szam < 10 then 1 else 1 + szamSzamjegyeinekSzama2 (szam `div` 10)
 
---4.feladat
+-- 4.feladat
 -- egy szám azon számjegyeinek összegét, mely paraméterként van megadva, pl. legyen a függvény neve fugv4, ekkor a következő meghívásra, a következő eredményt kell kapjuk:
 -- > fugv4 577723707 7
 -- 35
 szamAzonSzamjegyeinekOsszege :: Int -> Int -> Int
-szamAzonSzamjegyeinekOsszege szam szamjegy 
+szamAzonSzamjegyeinekOsszege szam szamjegy
   | szam < 10 = if szam == szamjegy then szam else 0
   | otherwise = if (szam `mod` 10) == szamjegy then (szam `mod` 10) + szamAzonSzamjegyeinekOsszege (szam `div` 10) szamjegy else szamAzonSzamjegyeinekOsszege (szam `div` 10) szamjegy
 
---labor 3 
+-- labor 3
+szamSzorzata1 n
+  | n < 10 = n
+  | otherwise = tmp1 * szamSzorzata1 tmp2
+  where
+    tmp1 = n `mod` 10
+    tmp2 = n `div` 10
 
-
+szamszorzata2 n = max n 1
+  where
+    aux n res
+      | n < 10 = res * n
+      | otherwise = aux tmp2 (res * tmp1)
+      where
+        tmp1 = n `mod` 10
+        tmp2 = n `div` 10
 
 main :: IO ()
 main = do
@@ -55,5 +71,5 @@ main = do
   print (szamSzamjegyeinekSzozata2 1234)
   putStr ("Szam szamjegyeinek osszege: ")
   print (szamSzemjegyeinekOsszege 1234)
-  putStr("Szam szamjegyeinek szama: ")
+  putStr ("Szam szamjegyeinek szama: ")
   print (szamSzamjegyeinekSzama 1234)
